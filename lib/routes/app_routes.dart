5d877a0389d60ee0332508fac3df32d643556ac6
WilliamBryann <williambryanp77@gmail.com>
2025-11-27 20:43:38 +0700

    Sign Up Page

--- lib/routes/app_routes.dart
+++ lib/routes/app_routes.dart
@@ -2,31 +2,34 @@ import 'package:flutter/material.dart';
 
 import 'package:uride/screen/home.dart';
 import 'package:uride/screen/sign_in.dart';
+import 'package:uride/screen/sign_up.dart';
 import 'package:uride/screen/search.dart';
+import 'package:uride/screen/lalulintas.dart';
 import 'package:uride/screen/lokasi_parkir.dart';
 import 'package:uride/screen/ubah_kata_sandi.dart';
 import 'package:uride/screen/verifikasi_kode.dart';
 import 'package:uride/screen/buat_password_baru.dart';
 
-
-
 class AppRoutes {
   static const home = '/home';
   static const signin = '/signin';
+  static const signup = '/signup';
   static const search = '/search';
   static const parking = '/parking';
   static const ubahKataSandi = '/ubah-kata-sandi';
   static const verifikasiKode = '/verifikasi-kode';
   static const buatPasswordBaru = '/buat-password-baru'; 
-
+  static const laluLintas = '/lalulintas';
 
 
   static Map<String, WidgetBuilder> routes = {
     home: (_) => const HomeScreen(),
     signin: (_) => const SignInPage(),
+    signup: (_) => const SignUpPage(),
     ubahKataSandi: (_) => const UbahKataSandiPage(),
     verifikasiKode: (_) => const VerifikasiKodePage(),
-    buatPasswordBaru: (_) => const BuatPasswordBaruPage(), 
+    buatPasswordBaru: (_) => const BuatPasswordBaruPage(),
+    laluLintas: (_)=> const LaluLintasPage(), 
   };
 
   static Route<dynamic>? generateRoute(RouteSettings settings) {
@@ -40,7 +43,9 @@ class AppRoutes {
       case verifikasiKode:
         return _animatedRoute(const VerifikasiKodePage());
       case buatPasswordBaru:
-      return _animatedRoute(const BuatPasswordBaruPage());
+        return _animatedRoute(const BuatPasswordBaruPage());
+      case laluLintas:
+        return _animatedRoute(const LaluLintasPage());
       default:
         return null;
     }
