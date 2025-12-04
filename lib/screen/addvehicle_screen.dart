86c20d8eab2d8290315261c0870cb2c530d7fee7
adljna <5026231068@student.its.ac.id>
2025-12-04 18:12:12 +0700

    Updated Route

--- lib/screen/addvehicle_screen.dart
+++ lib/screen/addvehicle_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:uride/routes/app_routes.dart';
 
 class TambahKendaraanPage extends StatefulWidget {
   const TambahKendaraanPage({super.key});
@@ -48,7 +49,7 @@ class _TambahKendaraanPageState extends State<TambahKendaraanPage> {
       );
 
       // Direct ke halaman VehicleDetailPage()
-      Navigator.pushReplacementNamed(context, "/vehicle-detail");
+      Navigator.pushReplacementNamed(context, AppRoutes.vehicle);
     } 
   }
 
